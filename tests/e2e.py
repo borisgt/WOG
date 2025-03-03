3dab1e0a2222325215436fc71c6d1110c290bda6
Boris Tian <boris.t@panorama.com>
2025-03-04 00:58:03 +0200

    feat: e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -21,19 +21,23 @@ def test_scores_service(app_url):
 
     return 1 <= score <= 1000
 
-def main_function(app_url):
+def main_function(url):
     """
     Main function to call the test_scores_service function.
-    :param app_url: URL of the web service
+    :param url: URL of the web service
     :return: -1 if the test fails, 0 if it passes.
     """
-    if app_url is None:
-        app_url = "http://localhost:8777"
+    if url is None:
+        url = "http://localhost:8777"
 
-    if test_scores_service(app_url):
+    if test_scores_service(url):
         print("Test passed!")
         sys.exit(0)
     else:
         print("Test failed!")
         sys.exit(-1)
+
+if __name__ == "__main__":
+    service_url = None
+    main_function(service_url)
 
